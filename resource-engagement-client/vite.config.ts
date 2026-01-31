b58fae5f21eb118cd9ca209b4fe2e338ee2c15ed
PiyushGitSierradev <piyush.singh@sierradev.com>
2026-01-31 22:47:49 +0530

    update url

--- resource-engagement-client/vite.config.ts
+++ resource-engagement-client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "https://localhost:5001",
+        target: "https://localhost:5000",
         changeOrigin: true,
         secure: false, // allow self-signed certs in dev
       },
